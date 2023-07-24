ed0aa90c4f351c8164c08aaba496c7662387e755
Rohan Patra <mohantyran@gmail.com>
2023-07-24 00:54:31 -0400

    Final version complete.

--- src/Derivative.java
+++ src/Derivative.java
@@ -1,44 +1,19 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-interface TermIterator {
-    boolean hasNext();
-
-    String next();
-}
-
-class PolynomialIterator implements TermIterator {
-    private final String[] terms;
-    private int index = 0;
-
-    public PolynomialIterator(String expression) {
-        this.terms = expression.split("\\+");
-    }
-
-    @Override
-    public boolean hasNext() {
-        return index < terms.length;
-    }
-
-    @Override
-    public String next() {
-        if (hasNext()) {
-            return terms[index++];
-        }
-        return null;
-    }
-}
-
+//An interface for parsing coefficient values from strings.
 interface CoefficientParser {
     double parse(String coefficient);
 }
-
+//A CoefficientParser implementation for parsing integer coefficients.
 class IntegerCoefficientParser implements CoefficientParser {
     @Override
     public double parse(String coefficient) {
         return Integer.parseInt(coefficient);
     }
 }
-
+//A CoefficientParser implementation for parsing fraction coefficients.
 class FractionCoefficientParser implements CoefficientParser {
     @Override
     public double parse(String coefficient) {
@@ -48,118 +23,17 @@ class FractionCoefficientParser implements CoefficientParser {
         return (double) numerator / denominator;
     }
 }
-
+//A CoefficientParser implementation for parsing decimal coefficients.
 class DecimalCoefficientParser implements CoefficientParser {
     @Override
     public double parse(String coefficient) {
         return Double.parseDouble(coefficient);
     }
 }
-
-interface CoefficientParserFactory {
-    CoefficientParser createParser(String coefficient);
-}
-
-class DefaultCoefficientParserFactory implements CoefficientParserFactory {
-    @Override
-    public CoefficientParser createParser(String coefficient) {
-        if (coefficient.contains("/")) {
-            return new FractionCoefficientParser();
-        } else if (coefficient.contains(".")) {
-            return new DecimalCoefficientParser();
-        } else {
-            return new IntegerCoefficientParser();
-        }
-    }
-}
-
-class Polynomial {
-    private final String expression;
-
-    public Polynomial(String expression) {
-        this.expression = expression;
-    }
-
-    public String getExpression() {
-        return expression;
-    }
-}
-
-class PolynomialDerivativeCalculator {
-    private final CoefficientParserFactory parserFactory;
-
-    public PolynomialDerivativeCalculator(CoefficientParserFactory parserFactory) {
-        this.parserFactory = parserFactory;
-    }
-
-    public String calculateDerivative(Polynomial polynomial, int order) {
-        String expression = polynomial.getExpression();
-
-        for (int i = 1; i <= order; i++) {
-            expression = calculateDerivative(expression);
-        }
-
-        return expression;
-    }
-
-    private String calculateDerivative(String expression) {
-        TermIterator iterator = new PolynomialIterator(expression);
-        StringBuilder derivative = new StringBuilder();
-
-        while (iterator.hasNext()) {
-            String term = iterator.next();
-            double coefficient = parseCoefficient(term);
-            double exponent = parseExponent(term);
-
-            String derivativeTerm = constructDerivativeTerm(coefficient, exponent);
-            derivative.append(derivativeTerm);
-            derivative.append(" + ");
-        }
-
-        // Remove the trailing "+"
-        derivative.setLength(derivative.length() - 3);
-
-        return derivative.toString();
-    }
-
-    private double parseCoefficient(String term) {
-        String coefficientStr = term;
-        if (term.contains("x")) {
-            coefficientStr = term.substring(0, term.indexOf("x"));
-            coefficientStr = coefficientStr.isEmpty() ? "1" : coefficientStr;
-        }
-        return parseCoefficientValue(coefficientStr);
-    }
-
-    private double parseExponent(String term) {
-        double exponent = 0;
-        if (term.contains("^")) {
-            String exponentStr = term.substring(term.indexOf("^") + 1);
-            exponent = Double.parseDouble(exponentStr);
-        }
-        return exponent;
-    }
-
-    private double parseCoefficientValue(String coefficient) {
-        CoefficientParser parser = parserFactory.createParser(coefficient);
-        return parser.parse(coefficient);
-    }
-
-    private String constructDerivativeTerm(double coefficient, double exponent) {
-        double newCoefficient = coefficient * exponent;
-        double newExponent = exponent - 1;
-
-        if (newExponent == 0) {
-            // The term has no x (constant term)
-            return String.valueOf(newCoefficient);
-        } else {
-            // The term has x with a non-zero exponent
-            return newCoefficient + "x^" + newExponent;
-        }
-    }
-}
-
+//A simple polynomial derivative calculator that calculates the nth order derivative of a polynomial.
 public class Derivative {
+    private static final Pattern termPattern = Pattern.compile("(-?\\d+(\\.\\d+)?/?\\d*(\\.\\d+)?)?x(\\^(-?\\d+(\\.\\d+)?))?");
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -172,17 +46,104 @@ public class Derivative {
         System.out.print("Enter the order of the derivative: ");
         int order = scanner.nextInt();
 
-        CoefficientParserFactory parserFactory = new DefaultCoefficientParserFactory();
-        PolynomialDerivativeCalculator calculator = new PolynomialDerivativeCalculator(parserFactory);
-
-        Polynomial polynomial = new Polynomial(expression);
-        String result = calculator.calculateDerivative(polynomial, order);
+        String result = calculateDerivative(expression, order);
 
         System.out.println("The " + order + "-th order derivative of the polynomial is: " + result);
 
         scanner.close();
     }
+    /**
+     * Calculates the nth order derivative of a given polynomial expression.
+     *
+     * @param expression The polynomial expression to calculate the derivative for.
+     * @param order      The order of the derivative to calculate.
+     * @return The resulting polynomial expression after taking the derivative.
+     */
+    public static String calculateDerivative(String expression, int order) {
+        for (int i = 0; i < order; i++) {
+            expression = calculateDerivative(expression);
+        }
+        return expression;
+    }
+    /**
+     * Calculates the derivative of a single term in the polynomial expression.
+     *
+     * @param expression The term to calculate the derivative for.
+     * @return The resulting derivative term.
+     */
+    public static String calculateDerivative(String expression) {
+        Matcher matcher = termPattern.matcher(expression);
+        StringBuilder derivative = new StringBuilder();
+
+        while (matcher.find()) {
+            String coefficientStr = matcher.group(1);
+            String exponentStr = matcher.group(5);
+
+            double coefficient = parseCoefficient(coefficientStr);
+            double exponent = parseExponent(exponentStr);
+
+            double newCoefficient = coefficient * exponent;
+            double newExponent = exponent - 1;
+
+            if (newCoefficient != 0) {
+                if (derivative.length() > 0) {
+                    derivative.append(" + ");
+                }
+
+                if (newExponent == 0) {
+                    derivative.append(newCoefficient);
+                } else if (newExponent == 1) {
+                    derivative.append(newCoefficient);
+                    derivative.append("x");
+                } else {
+                    derivative.append(newCoefficient);
+                    derivative.append("x^");
+                    derivative.append(newExponent);
+                }
+            }
+        }
+
+        return derivative.toString();
+    }
+
+    /**
+     * Parses the coefficient value from a string representation.
+     *
+     * @param coefficient The coefficient string to parse.
+     * @return The parsed coefficient value as a double.
+     */
+    private static double parseCoefficient(String coefficient) {
+        if (coefficient == null || coefficient.isEmpty() || coefficient.equals("-")) {
+            return 1.0;
+        }
+
+        CoefficientParser parser;
+        if (coefficient.contains("/")) {
+            parser = new FractionCoefficientParser();
+        } else if (coefficient.contains(".")) {
+            parser = new DecimalCoefficientParser();
+        } else {
+            parser = new IntegerCoefficientParser();
+        }
+
+        return parser.parse(coefficient);
+    }
+    /**
+     * Parses the exponent value from a string representation.
+     *
+     * @param exponent The exponent string to parse.
+     * @return The parsed exponent value as a double.
+     */
+    private static double parseExponent(String exponent) {
+        if (exponent == null || exponent.isEmpty()) {
+            return 1.0;
+        }
+
+        return Double.parseDouble(exponent);
+    }
 }
+
+
 
 
 
---
+++ src/DerivativeTest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DerivativeTest {
+
+    @Test
+    void testPolynomialDerivative() {
+        // Test case 1: Simple linear polynomial
+        String result1 = Derivative.calculateDerivative("2x + 3", 1);
+        assertEquals("2.0", result1);
+
+        // Test case 2: Quadratic polynomial
+        String result2 = Derivative.calculateDerivative("3x^2 + 2x + 1", 1);
+        assertEquals("6.0x + 2.0", result2);
+
+        // Test case 3: Higher-order polynomial
+        String result3 = Derivative.calculateDerivative("5x^3 + 4x^2 + 3x + 2", 2);
+        assertEquals("30.0x + 8.0", result3);
+
+        // Test case 4: Polynomial with fractional coefficients
+        String result4 = Derivative.calculateDerivative("1/2x^2 + 3/4x + 1/3", 1);
+        assertEquals("1.0x + 0.75", result4);
+
+        // Test case 5: Zero order derivative (original expression should be returned)
+        String result5 = Derivative.calculateDerivative("4x^3 + 2x^2 + x + 3", 0);
+        assertEquals("4x^3 + 2x^2 + x + 3", result5);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
